2a4d8aa0c28f20930cdfa569c555b4b4c663d431
Sainrahmani12 <husainrahmani127@gmail.com>
2022-01-15 21:51:28 +0700

    ganti

--- resources/views/user/isiuser.blade.php
+++ resources/views/user/isiuser.blade.php
@@ -100,8 +100,8 @@
                 </div>
                 <div class="card-body text-center">
                     <h2 class="dt-mobil1">{{$i->user->name}}</h2>
-                    <p class="dt-mobil1 mt-3">{{$i->datamobil->nama_mobil}}</p>
-                    <p class="dt-mobil1 mt-3">{{$i->datamobil?->nopol_mobil}}</p>
+                    <p class="dt-mobil1 mt-3">{{$i->datamobil?->nama_mobil}}</p>
+                    <p class="dt-mobil1 mt-3">{{$i->datamobil->nopol_mobil}}</p>
                     <p class="dt-mobil1 mt--3">{{ number_format((int)$i->datamobil->hargasewa_mobil * (int)$i->jumlah_hari)}},000</p>
                 </div>
             </div>
